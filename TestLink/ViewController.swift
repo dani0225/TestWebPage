6599e63e6707ee483aecd5a1aeadd449c6a83493
Daniel Ardeleanu <daniel.ardeleanu@ro.ibm.com>
2018-11-26 11:44:59 +0200

    additional implementation

--- TestLink/ViewController.swift
+++ TestLink/ViewController.swift
@@ -16,28 +16,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myWebView: WKWebView!
     
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         textFieldOutlet.delegate = self
+        myWebView.navigationDelegate = self
+        
        
     }
 
     func openWebPage() {
         if let userInsertedLink = textFieldOutlet.text {
-            guard let url = URL(string: userInsertedLink) else { return }
+            guard let url = URL(string: userInsertedLink) else {
+                showAlart(message: "cannot convert Url")
+                return }
             if UIApplication.shared.canOpenURL(url) {
                 let urlRequest:URLRequest = URLRequest(url: url)
                 myWebView.load(urlRequest)
             }
             else {
-                let alert = UIAlertController(title: "Error", message: "Please enter a valid URL.", preferredStyle: UIAlertController.Style.alert)
-                
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
+                showAlart(message: "Please enter a valid URL.")
             }
       
         }
+    }
+    
+    func showAlart(message:String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 
@@ -49,6 +58,47 @@ extension ViewController: UITextFieldDelegate {
         openWebPage()
         self.view.endEditing(true)
         return false
+    }
+    
+}
+
+extension ViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if(navigationResponse.response .isKind(of: HTTPURLResponse.self)){
+            let response = navigationResponse.response as! HTTPURLResponse
+            if(response.statusCode == 401){
+               showAlart(message: "401 Unauthorized")
+            }
+            else if (response.statusCode == 400) {
+                showAlart(message: "Bad Request")
+            }
+            else if (response.statusCode == 403) {
+                showAlart(message: "Forbidden")
+            }
+            else if (response.statusCode == 522) {
+                showAlart(message: "Connection timed out")
+            }
+            else if (response.statusCode == 524) {
+                showAlart(message: "A timeout occurred")
+            }
+            else if (response.statusCode == 504) {
+                showAlart(message: "Gateway Timeout")
+            }
+            else {
+                showAlart(message: "\(response.statusCode)")
+            }
+        }
+        decisionHandler(WKNavigationResponsePolicy.allow)
     }
 }
 
